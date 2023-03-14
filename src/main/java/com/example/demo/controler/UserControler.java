304473899c3c9a48c2d84a2304bdbcc12e1b8a31
liush <liushengjun97@163.com>
2023-03-14 17:42:40 +0800

    文件下载

--- src/main/java/com/example/demo/controler/UserControler.java
+++ src/main/java/com/example/demo/controler/UserControler.java
@@ -9,7 +9,9 @@ import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -144,5 +146,51 @@ public class UserControler {
 //        File file1=new File(fileName,path,time);
 //        fileService.addFile(file1);//调用service方法 将文件信息插入数据库
         return new ResponseEntity<>(DemoResponseCode.OK, "上传成功！");
+    }
+
+    /**
+     * 文件下载
+     * （将文件以流的形式一次性读取到内存，通过响应输出流输出到前端）
+     *
+     * @param path
+     * @param response
+     */
+    @GetMapping("/download")
+    public void download(String path, HttpServletResponse response) {
+        try {
+            // 本地测试路径 C:/Users/liush/Desktop/软考密码.txt  F:/测试文件上传/OIM流程.docx
+            // path是指想要下载的文件的路径
+            File file = new File(path);
+            //log.info(file.getPath());
+            // 获取文件名
+            String filename = file.getName();
+            // 获取文件后缀名
+            //String ext = filename.substring(filename.lastIndexOf(".") + 1).toLowerCase();
+            //log.info("文件后缀名：" + ext);
+
+            // 将文件写入输入流
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStream fis = new BufferedInputStream(fileInputStream);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fis.close();
+
+            // 清空response
+            response.reset();
+            // 设置response的Header
+            response.setCharacterEncoding("UTF-8");
+            //Content-Disposition的作用：告知浏览器以何种方式显示响应返回的文件，用浏览器打开还是以附件的形式下载到本地保存
+            //attachment表示以附件方式下载   inline表示在线打开   "Content-Disposition: inline; filename=文件名.mp3"
+            // filename表示文件的默认名称，因为网络传输只支持URL编码的相关支付，因此需要将文件名URL编码后进行传输,前端收到后需要反编码才能获取到真正的名称
+            response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, "UTF-8"));
+            // 告知浏览器文件的大小
+            response.addHeader("Content-Length", "" + file.length());
+            OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/octet-stream");
+            outputStream.write(buffer);
+            outputStream.flush();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
