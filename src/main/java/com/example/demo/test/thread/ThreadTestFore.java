efe174a86735b0e94a88fb116d3bea99d0e7ce8d
liush <liushengjun97@163.com>
2023-04-12 21:37:17 +0800

    加一种线程启动方式

--- src/main/java/com/example/demo/test/thread/ThreadTestFore.java
+++ src/main/java/com/example/demo/test/thread/ThreadTestFore.java
@@ -9,10 +9,10 @@ public class ThreadTestFore {
 
         Thread thread1 = new Thread(threadFour1, "线程1");
         Thread thread2 = new Thread(threadFour2, "线程2");
-        Thread thread3 = new Thread(threadFour3, "线程3");
+
+        new Thread(threadFour3, "线程3").start();
 
         thread1.start();
         thread2.start();
-        thread3.start();
     }
 }
